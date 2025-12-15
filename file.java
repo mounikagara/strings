7357c9fa0c0c03e170399e4b64fb150eb67cd319
mounikagara <mounikagara44@gmail.com>
2025-12-15 13:15:39 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+public class main{
+  public static void main(String[] args){
+    String input="Hello";
+    Srting X = "";
+            for (int i=input.length()-1; i>=0 ; i--) {
+              X+=input.charAt(i);
+            }
+    System.out.println(X);
+  }
+}
+
+              
+              
 
